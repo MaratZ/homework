39489e823276d73e3a772ac0e88b135bdf35d111
Mara <Zainullin.marat93@yandex.ru>
2024-08-09 17:13:44 +0300

    9.2.2

---
+++ src/widget.py
@@ -0,0 +1,24 @@
+from src.masks import get_mask_account_card
+
+
+def mask_account_card(number: str) -> str:
+    #"""Функция для маскировки счетов и карт"""
+    if len(number.split()[-1]) == 16:
+        new_number = card_number(number.split()[-1])
+        result = f"{number[:-16]}{new_number}"
+        return result
+    elif len(number.split()[-1]) == 20:
+        new_number = get_mask_account_card(number.split()[-1])
+        result = f"{number[:-20]}{new_number}"
+    return result
+
+
+def get_date(date: str) -> str:
+    """Функция преобразования даты и времени"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
+    
+    
+#print(get_date('2024-07-11T02:26:18.671407'))
+    if __name__ == '__main__':
+        print(mask_account_card('Счет 12345678901234567890'))
+        print(mask_account_card('Visa Platinum 1234567890123456'))
