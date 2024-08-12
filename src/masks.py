3735ec979878699bd17ca0c6e61cd6302cc743b6
Mara <Zainullin.marat93@yandex.ru>
2024-08-12 14:18:06 +0300

    add processing

--- src/masks.py
+++ src/masks.py
@@ -1,21 +1,11 @@
-def mask_card(card_number: str) -> str | None:
+
+def get_mask_card(card_number: str) -> str:
     """Функция маскировки карты"""
-    if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
-    else:
-        return None
+    return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
 
 
-def mask_account(acc_number: str) -> str | None:
+def get_mask_account(acc_number: str) -> str:
     """Функцию маскировки номера банковского счета"""
-    if acc_number.isdigit() and len(acc_number) == 20:
-        return f"{'*' * 2}{acc_number[-4::]}"
-    else:
-        return None
+    return f"{'*' * 2}{acc_number[-4::]}"
 
-
-def get_mask_account_card():
-    if __name__ == '__main__':
-        print(mask_account_card('Счет 12345678901234567890'))
-        print(mask_account_card('Visa Platinum 1234567890123456'))
-        return None
+#print(get_mask_card("1234567891234567"))
