ecc858661259c282afd7b774786121c37ce104e0
Mara <Zainullin.marat93@yandex.ru>
2024-08-30 14:49:52 +0300

    12.1.1

--- main.py
+++ main.py
@@ -27,3 +27,6 @@ def card_number_generator(start: int, stop: int) -> Iterator[str]:
         card_number = f"{x:016}"
         formatted_number = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
         yield formatted_number
+
+
+
